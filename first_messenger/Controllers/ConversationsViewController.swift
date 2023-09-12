b96bf438b20175c644c27cdf17727484aa328e73
Alex Kornilov <alex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.com>
2023-09-13 01:07:12 +0300

    made a Log In and Register field

--- first_messenger/Controllers/ConversationsViewController.swift
+++ first_messenger/Controllers/ConversationsViewController.swift
@@ -11,8 +11,31 @@ class ConversationsViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.backgroundColor = .red
 		
 	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+			super.viewDidAppear(animated)
+		let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+		if  !isLoggedIn {
+			let vc = LoginViewController()
+			let nav = UINavigationController(rootViewController: vc)
+			nav.modalPresentationStyle = .fullScreen
+			present(nav, animated: false)
+		}
+			//validateAuth()
+		}
+
+//		private func validateAuth() {
+//			let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+//			if  !isLoggedIn {
+//				let vc = LoginViewController()
+//				let nav = UINavigationController(rootViewController: vc)
+//				nav.modalPresentationStyle = .fullScreen
+//				present(nav, animated: false)
+//			}
+//		}
 
 
 }
